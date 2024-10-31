2ec9ab36e1fe880ed460b0249d39ec7697fcd780
Yelyzaveta Skorohliad <skoroglyadliza@gmail.com>
2024-10-31 02:17:16 +0200

    Add go-back button and add genres

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,15 +1,20 @@
 import styles from "./MovieList.module.css"
 import noImage from "../../images/noImage.png"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 const MovieList = ({ movies }) => {
+
+    const location = useLocation()
+
     return (
         <div>
             <ul className={styles.listOfMovies}>
                 {movies && movies.map((movie) => {
                     return (
                         <li className={styles.movieCard} key={movie.id}>
-                            <Link to={`/movies/${movie.id}`}>
+                            <Link to={`/movies/${movie.id}`}
+                                state={{ from: location }}
+                            >
                                 <div className={styles.imgWrapper}>
                                     <img
                                         src={movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : noImage}
--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,17 +1,24 @@
 // import MovieCast from "../../components/MovieCast/MovieCast"
 // import MovieReviews from "../../components/MovieReviews/MovieReviews"
 
-import { useEffect, useState } from "react"
-import { NavLink, Outlet, useParams } from "react-router-dom"
+import { useEffect, useRef, useState } from "react"
+import { Link, NavLink, Outlet, useLocation, useNavigate, useParams } from "react-router-dom"
 import { oneMovieDetails } from "../../api/movies"
 import Loader from "../../components/Loader/Loader"
 
 import styles from "./MovieDetailsPage.module.css"
 import clsx from "clsx"
 import noImage from "../../images/noImage.png"
+import svg from "../../images/icons.svg"
 
 
 const MovieDetailsPage = () => {
+
+    const location = useLocation()
+    const backLink = useRef(location.state?.from || "/movies")
+    const navigate = useNavigate()
+    const goBack = () => navigate(backLink)
+
     const { movieId } = useParams()
     const [movieDetails, setMovieDetails] = useState(null)
     const [isLoading, setIsLoading] = useState(false)
@@ -48,12 +55,27 @@ const MovieDetailsPage = () => {
                             alt="Movie's poster" />
                     </div>
                     <div>
-                        <h2 className={styles.title}>{movieDetails.title}</h2>
+                        <div className={styles.buttonAndTitle}>
+                            <Link to={backLink.current}>
+                                <button onClick={goBack} className={styles.backButton}>
+                                    <svg width="22px" height="22px">
+                                        <use href={`${svg}#icon-arrow-left2`}></use>
+                                    </svg>
+                                </button>
+                            </Link>
+                            <h2 className={styles.title}>{movieDetails.title}</h2>
+                        </div>
                         <p className={styles.movieText}><span className={styles.describtionText}>Description:</span> {movieDetails.overview}</p>
                         <p className={styles.movieText}><span className={styles.describtionText}>Reliese Date: </span>{movieDetails.release_date}</p>
+                        <p className={styles.movieText}>
+                            <span className={styles.describtionText}>Genres: </span>
+                            {movieDetails.genres.map((genre, index) => (
+                                <span key={index}>{genre.name}{index < movieDetails.genres.length - 1 ? ', ' : ''}</span>
+                            ))}
+                        </p>
                         <ul className={styles.reviewsAndCast}>
-                            <li><NavLink className={({ isActive }) => clsx(styles.link, isActive && styles.activeLink)} to="cast">Cast</NavLink></li>
-                            <li><NavLink className={({ isActive }) => clsx(styles.link, isActive && styles.activeLink)} to="reviews">Reviews</NavLink></li>
+                            <li><NavLink state={{ from: backLink }} className={({ isActive }) => clsx(styles.link, isActive && styles.activeLink)} to="cast">Cast</NavLink></li>
+                            <li><NavLink state={{ from: backLink }} className={({ isActive }) => clsx(styles.link, isActive && styles.activeLink)} to="reviews">Reviews</NavLink></li>
                         </ul>
                         <Outlet />
                     </div>
